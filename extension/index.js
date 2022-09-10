e0d9ec3b58a2eaa3b721f9a967c6bc93ce3f3592
Kacper Pijacki <pij@cki.ct8.pl>
2022-09-10 02:11:32 +0200

    add support to detect dark mode in normal opera

--- extension/index.js
+++ extension/index.js
@@ -3,6 +3,9 @@ port.onMessage.addListener(function (message, sender) {
 	console.log(message, sender);
 	if (message.mode === "loadGXTheme") {
 		document.documentElement.style = message.gx;
+		message.theme === "dark" ? loadCSS("css/GXSkin") : unloadCSS("css/GXSkin");
+	} else if (message.mode === "loadTheme") {
+		message.theme === "dark" ? loadCSS("css/DarkSkin") : unloadCSS("css/DarkSkin");
 	}
 });
 
@@ -12,40 +15,31 @@ document.addEventListener("readystatechange", () => {
 		loadCSS("css/DefaultFix");
 
 		chrome.runtime.sendMessage({ mode: "getTheme" }, (response) => {
-			if (response.theme === "dark") {
-				if (response.gx !== null) {
-					document.documentElement.style = response.gx;
-					loadCSS("css/GXSkin");
-				} else {
-					loadCSS("css/DarkSkin");
-				}
+			console.log(response);
+			if (response.gx !== "") {
+				document.documentElement.style = response.gx;
+				response.theme === "dark" && loadCSS("css/GXSkin");
+			} else {
+				response.theme === "dark" && loadCSS("css/DarkSkin");
 			}
 		});
 
+		let blockHidePanel = false;
+
 		var checkExist = setInterval(function () {
-			if (document.querySelectorAll(".hybvsw6c > .rj1gh0hx").length) {
-				document.querySelector(".hybvsw6c > .rj1gh0hx").classList.add("switchTheme");
-				document.querySelector(".switchTheme").innerHTML = `
-				<i
-					data-visualcompletion="css-img"
-					class="hu5pjgll lzf7d6o1"
-					style="
-						background-image: url('https://static.xx.fbcdn.net/rsrc.php/v3/yT/r/Q8CfqKxqizP.png');
-						background-position: 0px -335px;
-						background-size: auto;
-						width: 20px;
-						height: 20px;
-						background-repeat: no-repeat;
-						display: inline-block;
-						margin: auto;
-					"
-				></i>
-				`;
-				document.querySelector(".switchTheme").addEventListener("click", () => {
-					switchTheme();
-				});
-				clearInterval(checkExist);
-			}
+			if (!document.querySelector(`input[dir="ltr"]`)) return;
+
+			document.querySelector(`input[dir="ltr"]`).addEventListener("focus", () => {
+				document.querySelector(`div[role="navigation"]`).classList.add("fullWidth");
+				blockHidePanel = true;
+			});
+
+			document.querySelector(`input[dir="ltr"]`).addEventListener("blur", () => {
+				document.querySelector(`div[role="navigation"]`).classList.remove("fullWidth");
+				blockHidePanel = false;
+			});
+
+			clearInterval(checkExist);
 		}, 100);
 
 		let leftPanelState = 1;
@@ -53,7 +47,7 @@ document.addEventListener("readystatechange", () => {
 			if (e.clientX < 10 && leftPanelState === 0) {
 				document.querySelector(`div[role="navigation"]`).classList.remove("zeroWidth");
 				leftPanelState = 1;
-			} else if (window.innerWidth < 900 && e.clientX > 122 && leftPanelState === 1) {
+			} else if (!blockHidePanel && window.innerWidth < 900 && e.clientX > 122 && leftPanelState === 1) {
 				document.querySelector(`div[role="navigation"]`).classList.add("zeroWidth");
 				leftPanelState = 0;
 			}
@@ -71,23 +65,6 @@ function loadCSS(file) {
 }
 
 function unloadCSS(file) {
-	var cssNode = document.getElementById(file);
-	cssNode && cssNode.parentNode.removeChild(cssNode);
-}
-
-function switchTheme() {
-	chrome.runtime.sendMessage({ mode: "getTheme" }, (response) => {
-		if (response.theme === "dark") {
-			unloadCSS(response.gx !== null ? "css/GXSkin" : "css/DarkSkin");
-		} else {
-			if (response.gx !== null) {
-				document.documentElement.style = response.gx;
-				loadCSS("css/GXSkin");
-			} else {
-				loadCSS("css/DarkSkin");
-			}
-		}
-
-		chrome.runtime.sendMessage({ mode: "saveTheme", theme: response.theme === "dark" ? "light" : "dark" });
-	});
+	var cssNode = document.querySelectorAll(`link[id="${file}"]`);
+	cssNode.forEach((elm) => elm.parentNode.removeChild(elm));
 }
